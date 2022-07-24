bd7ea63dfd55849bfa11fa4e385b8e8cac8de1c4
nesaruddinisca <nesaruddinisca1992@gmail.com>
2022-07-24 09:40:15 +0600

    Angular form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+export interface User{
+  username:string,
+  password:string
+}
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Exam_form';
+
+name:any="";
+pass:any="";
+
+user:User={
+  username:'',
+  password:""
+}
+constructor(){
+  this.user={} as User;
+}
+myForm=new FormGroup({
+  username:new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(10)]),
+  password:new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(10)])
+})
+get username(){
+  return this.myForm.get('username')
+}
+get password(){
+  return this.myForm.get('password')
+}
+onSubmit(){
+  console.log(this.myForm.value);
+  this.name=this.myForm.value.username;
+  this.pass=this.myForm.value.password;
+  
+}
+
+
+
+
 }
